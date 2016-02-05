72195e8e2262dfe427b036d2856ed79e8147d6f6
费凡 <404619986@qq.com>
2016-02-05 16:19:50 +0800

    fix

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Android绘制热力图
 * HeatMap 使用高斯核密度估计方法绘制
 * HeatMapOverlay 使用绘制点阴影的方式绘制
+
+![main](/screenshot/main.png)
--- app/src/main/java/com/ff/heatmap/MainActivity.java
+++ app/src/main/java/com/ff/heatmap/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -31,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("热力图");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         measureScreen();
         List<WeightedLatLng> data = generateHeatMapData();
-        heatMap = new HeatMap.Builder().weightedData(data).width(screenWidth).height(screenHeight).build();
+        heatMap = new HeatMap.Builder().weightedData(data).radius(35).width(screenWidth).height(screenHeight).build();
         imageView.setImageBitmap(heatMap.generateMap());
 
     }
@@ -67,33 +66,11 @@ public class MainActivity extends AppCompatActivity {
     @NonNull
     private List<WeightedLatLng> generateHeatMapData() {
         List<WeightedLatLng> data = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             data.add(new WeightedLatLng((int) (Math.random() * screenWidth),
                     (int) (Math.random() * screenHeight),
                     Math.random() * 100));
         }
         return data;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
